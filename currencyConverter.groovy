744a0271562b1ac424535844b6c11b59632aba85
Mr Ian Robinson <irobin02@dcs.bbk.ac.uk>
2016-10-10 19:05:19 +0100

    my commit message

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,3 +1,4 @@
+println "THIS IS A AN EDIT FOR GIUTHUB LEARNING"
 print "If I give you £1, how many Euro will you give me? ";
 double euroOverPoundRatio = Double.parseDouble(System.console().readLine());
 print "If I give you 1€, how many Sterling pounds will you give me? ";
